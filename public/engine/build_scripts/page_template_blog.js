d999fcc9d0d74d884a7c272d8d22fd09248df469
saladinid <didien.geonk@gmail.com>
2019-08-02 16:42:59 +0800

    add bimbel

--- public/engine/build_scripts/page_template_blog.js
+++ public/engine/build_scripts/page_template_blog.js
@@ -50,6 +50,15 @@ module.exports = {
     </script>
     <script src="../assets/script/lib/nav/modernizr.custom.js">
     </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-145097646-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-145097646-1');
+    </script>
   </head>
   <body>
   <header id="header">
